92f9a7798bbd6a33a0717e6382376760874171d1
Stella Kim <stellahkim93@gmail.com>
2019-02-07 18:31:01 -0500

    scraped URLs for women's pants/tights

--- ll_tights_urls.py
+++ ll_tights_urls.py
@@ -8,7 +8,6 @@ import csv
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import re
 
 # Use Chrome browser
 driver = webdriver.Chrome()
@@ -16,9 +15,9 @@ driver = webdriver.Chrome()
 driver.get('https://shop.lululemon.com/c/women-pants/_/N-1z109yvZ7yh')
 
 # Writing CSV file
-#csv_file = open('ll_urls.csv', 'w', encoding= 'utf-8')
-#writer = csv.writer(csv_file)
-#writer.writerow()
+csv_file = open('ll_urls.csv', 'w', encoding= 'utf-8')
+writer = csv.writer(csv_file)
+writer.writerow(['product_name', 'product_url'])
 
 # Selenium script to scroll to the bottom, wait 3 seconds for the next batch of data to load, then continue scrolling.  It will continue to do this until the page stops loading new data.
 lenOfPage = driver.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
@@ -33,31 +32,19 @@ while(match==False):
 try:
 	products = driver.find_elements_by_xpath('//div[@class=" product-tile"]')
 
-	product_urls = []
 	for product in products:
-		#product_name = product.find_element_by_xpath('.//div[@class="product-display-name"]//h3[@class="product-name"]').text.replace("\n", " ")
+		product_dict = {}
+		product_name = product.find_element_by_xpath('.//div[@class="product-display-name"]//h3[@class="product-name"]').text.replace("\n", " ")
 		#product_price = product.find_element_by_xpath('.//span[@class="product-price"]').text.replace("D$", "D, $")
 		product_url = product.find_element_by_xpath('.//div[@class="product-display-name"]/a').get_attribute("href")
 
-
-		#product_list['product_name'] = product_name
+		product_dict['product_name'] = product_name
 		#product_list['product_price'] = product_price
-		product_urls.append(product_url)
+		product_dict['product_url'] = product_url
 
-		#print(product_urls)
-
+		writer.writerow(product_dict.values())
 
 except Exception as e:
 	print(e)
 	driver.close()
-
-
-print(product_urls)
-
-#product_info = wait_time.until(EC.presence_of_all_elements_located((By.XPATH, '//div[@class="product-description"]')))
-#product_name = product_info.find_element_by_xpath('.//h1[@class="pdp-title"]').text
-#product_price = product_info.find_element_by_xpath('.//span[@class="list-price"]').text
-
-# not sure if i'm allowed to use product image
-#product_image = product_info.find_element_by_xpath('//img[@class="default-image show-image swiper-slide swiper-slide-active"]')
-
+	csv_file.close()
