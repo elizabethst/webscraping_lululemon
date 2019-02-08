a78a9f558fcbf33965398423ced33b90ad0186ec
Stella Kim <stellahkim93@gmail.com>
2019-02-08 08:21:39 -0500

    updating urls

--- ll_tights_urls.py
+++ ll_tights_urls.py
@@ -17,14 +17,13 @@ driver.get('https://shop.lululemon.com/c/women-pants/_/N-1z109yvZ7yh')
 # Writing CSV file
 csv_file = open('ll_urls.csv', 'w', encoding= 'utf-8')
 writer = csv.writer(csv_file)
-writer.writerow(['product_name', 'product_url'])
 
 # Selenium script to scroll to the bottom, wait 3 seconds for the next batch of data to load, then continue scrolling.  It will continue to do this until the page stops loading new data.
 lenOfPage = driver.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
 match=False
 while(match==False):
 	lastCount = lenOfPage
-	time.sleep(3)
+	time.sleep(1.5)
 	lenOfPage = driver.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
 	if lastCount==lenOfPage:
 		match=True
@@ -33,18 +32,13 @@ try:
 	products = driver.find_elements_by_xpath('//div[@class=" product-tile"]')
 
 	for product in products:
-		product_dict = {}
-		product_name = product.find_element_by_xpath('.//div[@class="product-display-name"]//h3[@class="product-name"]').text.replace("\n", " ")
-		#product_price = product.find_element_by_xpath('.//span[@class="product-price"]').text.replace("D$", "D, $")
 		product_url = product.find_element_by_xpath('.//div[@class="product-display-name"]/a').get_attribute("href")
-
-		product_dict['product_name'] = product_name
-		#product_list['product_price'] = product_price
-		product_dict['product_url'] = product_url
-
-		writer.writerow(product_dict.values())
+		writer.writerow([product_url])
 
 except Exception as e:
 	print(e)
 	driver.close()
 	csv_file.close()
+
+
+print("\nDone scraping product URLs\n")
